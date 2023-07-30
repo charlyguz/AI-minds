5e9c2f50b86965b218cb2fb984c804c0556c82f1
charlyguz <guzmancarl5610@gmail.com>
2023-07-29 23:38:47 -0600

    comentarios de la logica principal

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,13 +1,66 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+#import openai_secret_manager #esta importacion causa unos problemitas revisen sus instalaciones antes de correr el servidos
+import openai
+import requests
+
+#assert "openai" in openai_secret_manager.get_services()
+#secrets = openai_secret_manager.get_secret("openai")
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api', methods=['GET'])
-def welcome():
-    response = {"message": "Bienvenido a la API!"}
+@app.route('/process_audio', methods=['POST'])
+def process_audio():
+    audio_file = request.files['audio']
+    # Utiliza Whisper para convertir el audio a texto
+    text = transcribe_audio(audio_file)
+    # Utiliza GPT para analizar el texto y generar retroalimentación
+    feedback = get_feedback(text)
+    response = {"message": feedback}
     return jsonify(response)
+
+def transcribe_audio(audio_file):
+    # Aquí iría el código para enviar una solicitud a la API de Whisper
+    # y convertir el archivo de audio en texto
+    response = requests.post(
+        "https://api.openai.com/v1/engines/davinci-coding/completions",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {secrets['api_key']}"
+        },
+        data={
+            "prompt": f"Transcribe this audio file into text: {audio_file}",
+            "max_tokens": 1024,
+            "n": 1,
+            "stop": None,
+            "temperature": 0.5,
+        }
+    )
+    data = response.json()
+    text = data['choices'][0]['text']
+    return text
+
+def get_feedback(text):
+    # Aquí iría el código para enviar una solicitud a la API de GPT-4
+    # y obtener retroalimentación a partir del texto transcrito
+    response = requests.post(
+        "https://api.openai.com/v1/engines/davinci-coding/completions",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {secrets['api_key']}"
+        },
+        data={
+            "prompt": f"Analyze this text and provide feedback: {text}",
+            "max_tokens": 1024,
+            "n": 1,
+            "stop": None,
+            "temperature": 0.5,
+        }
+    )
+    data = response.json()
+    feedback = data['choices'][0]['text']
+    return feedback
 
 if __name__ == '__main__':
     app.run(debug=True)
--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,28 +1,68 @@
-import React, { useState, useEffect } from "react";
-import index from "./index.css";
+import React, { useState } from "react";
 import teacher from './assets/teacher.png';
 
-
 export default function App() {
-  useEffect(() => {
-    fetch('http://localhost:5000/api')
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }, []);
+  const [recording, setRecording] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [responseText, setResponseText] = useState(""); // Almacenar la respuesta de GPT
+
+  const startRecording = () => {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+      const mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      setIsRecording(true);
+
+      const audioChunks = [];
+      mediaRecorder.addEventListener("dataavailable", event => {
+        audioChunks.push(event.data);
+      });
+
+      mediaRecorder.addEventListener("stop", () => {
+        const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+        sendAudioToServer(audioBlob);
+        setRecording(audioBlob);
+      });
+
+      setRecording(mediaRecorder);
+    });
+  };
+
+  const stopRecording = () => {
+    if (recording) {
+      recording.stop();
+      setIsRecording(false);
+    }
+  };
+
+  const sendAudioToServer = async (audioBlob) => {
+    const formData = new FormData();
+    formData.append('audio', audioBlob);
+
+    const response = await fetch('http://localhost:5000/process_audio', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await response.json();
+    setResponseText(data.message); // Guardar la respuesta en el estado
+  };
+
   return (
     <div className="">
       <img src="https://img.freepik.com/vector-gratis/interior-aula-escuela-universidad-concepto-educativo-pizarra-tabla_1441-1694.jpg?w=1480&t=st=1690667104~exp=1690667704~hmac=ee01a1dd45a569f0fdae751ae561ba032ade82af1a22743a54c0a3310d65b22c" alt="Fondo" className="absolute w-full h-full object-cover" />
-      
       <img src={teacher} alt="Personaje" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 w-1/2 md:w-1/3" />
-
-
-
       <div className="absolute bottom-0 left-0 w-full h-2/4 bg-black bg-opacity-50 p-4 rounded-t">
         <h1 className="text-6xl font-bold mb-4 text-black text-center">Hello I'am your teacher</h1>
-        <textarea className="w-full p-2 mb-4 h-40" readOnly value="Aqui va la respuesta de gpt en los 2 idiomas en el idioma natal y el que quiere aprender" />
-        <input type="text" className="w-full p-2" placeholder="Introduce tu texto aquí" />
+        <textarea className="w-full p-2 mb-4 h-40" readOnly value={responseText} />
+        <div className="flex space-x-4">
+          <input type="text" className="w-full p-2" placeholder="Introduce tu texto aquí" />
+          {isRecording ? (
+            <button onClick={stopRecording} className="bg-red-500 text-white p-2 rounded">Stop</button>
+          ) : (
+            <button onClick={startRecording} className="bg-blue-500 text-white p-2 rounded">Record</button>
+          )}
+        </div>
       </div>
     </div>
   );
 }
-
