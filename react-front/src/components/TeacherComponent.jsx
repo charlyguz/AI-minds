d4b6087666463aaa5d5a8fb842992a0cc16769df
GerardoS-C <gsandovalc1900@alumno.ipn.com>
2023-08-01 01:27:24 -0600

    implementacion de feedback

--- react-front/src/components/TeacherComponent.jsx
+++ react-front/src/components/TeacherComponent.jsx
@@ -5,6 +5,7 @@ import MicRecorder from 'mic-recorder-to-mp3';
 
 export default function TeacherComponent({ userData }) {
   const [responseText, setResponseText] = React.useState('');
+  const [feedbackText, setFeedback] = React.useState('');
   const recorder = useRef(null); //Recorder
   const audioPlayer = useRef(null); //Ref for HTML Audio tag
   const [transcriptionText, setTranscriptionText] = useState("");
@@ -55,6 +56,7 @@ export default function TeacherComponent({ userData }) {
       const audio = new Audio(`data:audio/wav;base64,${data.audio_response}`);
       const transcriptionWithUserName = `${userData.name}: ${data.transcript}`;
       setTranscriptionText(transcriptionWithUserName);
+      setFeedback(data.feedback);
       audio.play();
     };
     return (
@@ -62,7 +64,8 @@ export default function TeacherComponent({ userData }) {
         <img src={teacher} alt="Personaje" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 w-1/2 md:w-1/3" />
         <div className="absolute bottom-0 left-0 w-full h-2/4 bg-black bg-opacity-50 p-4 rounded-t">
           <h1 className="text-6xl font-bold mb-4 text-white text-center">Hello I'am your teacher</h1>
-          <textarea className="w-full p-2 mb-4 h-40" readOnly value={responseText} />
+          <textarea className="w-full p-2 mb-4 h-20" readOnly value={responseText}/>
+          <textarea className="w-full p-2 mb-4 h-20" readOnly value={feedbackText}/>
           <div className="flex space-x-4">
           <textarea type="text" className="w-full p-2" placeholder="Introduce tu texto aquí" value={transcriptionText}/>
           <button onClick={startRecording} disabled={isRecording}>
